999821b0672c3d65343d1ec8b45570f82be7921e
Daniel Manrique <roadmr@ubuntu.com>
2019-05-17 12:09:12 -0400

    some completion refinements

--- complete2.sh
+++ complete2.sh
@@ -1,16 +1,37 @@
-# last -> final
-# lang  -> tongue
+#
+#
+# "Globals"
+declare -A emojis
+declare emoji_shortnames
+
+# Functions
+load_emoji(){
+    while IFS=, read -r -a temp_array
+    do
+        # skip empty pairs
+        ((${#temp_array[@]} >= 2)) || continue
+
+        # skip empty keys (even if they have value)
+        if [ -n "${temp_array[@]:1:1}" ]; then
+            # assign associative
+            emojis["${temp_array[@]:1:1}"]="${temp_array[@]:0:1}"
+        fi
+    done < filtered.txt
+    emoji_shortnames=$(echo "${!emojis[@]}" )
+}
 
 comple(){
-    COMPREPLY=( $(compgen -W "last lang" "${COMP_WORDS[$COMP_CWORD]}") )
-
+    COMPREPLY=( $(compgen -W "$emoji_shortnames" "${COMP_WORDS[$COMP_CWORD]}") )
+    # TODO: Also resolve if the top match is identical to current word.
+    # This helps resolving word to emoji when there are still more matches but
+    # I have what I need.
     if [[ ${#COMPREPLY[@]} -eq 1 ]]; then
-        if [[ ${COMPREPLY[0]} = "last" ]]; then 
-            COMPREPLY=( :final: )
-        fi
-        if [[ ${COMPREPLY[0]} = "lang" ]]; then 
-            COMPREPLY=( :tongue: )
-        fi
+        local key=${COMPREPLY[0]}
+        local value=${emojis[$key]}
+        COMPREPLY=( $value )
     fi
 }
+
+# The meat
+load_emoji
 complete -F comple emoju
