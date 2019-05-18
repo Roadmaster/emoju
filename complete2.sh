badd80c1727293f0c10d7ad916d80f8f7236d38b
Daniel Manrique <roadmr@ubuntu.com>
2019-05-18 09:27:52 -0400

    Some misguided attempt to using COMP_TYPE to resolve final matches

--- complete2.sh
+++ complete2.sh
@@ -20,11 +20,19 @@ load_emoji(){
     emoji_shortnames=$(echo "${!emojis[@]}" )
 }
 
+LAST_COMP_TYPE=0
 comple(){
     COMPREPLY=( $(compgen -W "$emoji_shortnames" "${COMP_WORDS[$COMP_CWORD]}") )
+
     # TODO: Also resolve if the top match is identical to current word.
     # This helps resolving word to emoji when there are still more matches but
     # I have what I need.
+    #if [ $LAST_COMP_TYPE = 63 ] && [ $COMP_TYPE = 63 ]; then
+    #    echo "RESOLVE TO ${COMPREPLY[0]}"
+    #fi
+    #LAST_COMP_TYPE=$COMP_TYPE
+    # If we only have one key match, resolve the matching emoji value and
+    # COMPREPLY that
     if [[ ${#COMPREPLY[@]} -eq 1 ]]; then
         local key=${COMPREPLY[0]}
         local value=${emojis[$key]}
